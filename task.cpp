97545f86676e3a0eeb1b05bbc1976ca14c980867
Divyang20040709 <divyangsolanki2004@gmail.com>
2025-02-01 11:09:02 +0530

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,31 @@
+// using array login ,salary of employe,leave day, calculate thaya pachhi badhu aavu joiye salary and all like database
+//per day salary is 1000 rs
+ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numEmployees;
+    cout << "Enter number of employees: ";
+    cin >> numEmployees;
+
+    int salary[numEmployees];
+    int leaveDays[numEmployees];
+    const int dailySalary = 1000;
+
+    for (int i = 0; i < numEmployees; i++) {
+        cout << "Enter leave days for employee " << (i + 1) << ": ";
+        cin >> leaveDays[i];
+
+        // Calculate salary after leave deduction
+        salary[i] = (30 - leaveDays[i]) * dailySalary;
+    }
+
+    cout << "\nFinal Salary of Employees:\n";
+    for (int i = 0; i < numEmployees; i++) {
+        cout << "Employee " << (i + 1) << ": Rs. " << salary[i] << endl;
+    }
+
+    return 0;
+}
